d2a08d8c8d254fd73551b3fa1fc96580e3bcdbfe
n-tsunemi <tsunemin@gmail.com>
2019-04-15 21:23:21 +0900

    modified location file

--- config/application.rb
+++ config/application.rb
@@ -13,7 +13,10 @@ module Habilidad
 
     config.time_zone = 'Tokyo'
 
+    # locale
+    # config.i18n.default_locale = :en
     config.i18n.default_locale = :ja
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
