bb7b123f649584ed4bf28bf48dee8054f5d7938d
n-tsunemi <tsunemin@gmail.com>
2019-04-30 00:27:21 +0900

    habilidad-logo.png top logo.
    header.css
    navbar-brand link not display under line.
    
    top.css
    add.
    
    top.mp4
    background movie for top view.
    
    top_controller.rb
    add.
    
    top_helper.rb
    add.
    
    _header.html.erb
    navbar-brand to link.
    
    index.html.erb
    top view.
    
    routes.rb
    add top#index route.
    
    assets.rb
    add assets.
    
    en.yml
    ja.yml
    add.
    
    top_controller_test.rb
    add.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /#{I18n.available_locales.map(&:to_s).join('|')}/ do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    root 'top#index'
+
+    # top_controller
+    get 'top', to: 'top#index'
+
+    # businesses_controller
     resources :businesses
 
+    # clients_controller
     resources :clients
 
+    # certifications_controller
     resources :certifications
   end
 end
